7ba2849aed53d17537afb5483572a619a82a3729
Fabian Hartmann <git@hartmann.red>
2020-01-13 22:23:19 +0100

    debuig print()s auskommentiert

--- korrelation.py
+++ korrelation.py
@@ -15,13 +15,17 @@ def parsen():
     #Zahl fue dir id generieren. Diese ist der unique identifier 
     #fuer eine Korrelation. Aktuell sind es 86816 erzeugte Korrelationsgrafiken.
     cor_id = str(random.randint(1, 86816))
-    print(cor_id)
+
+    #/debug
+    #print(cor_id)
 
     #GET Request fuer die erzeugte korrelation
     #ReOb ist jetzt die HTML Antwort
     url = "https://tylervigen.com/view_correlation?id="+cor_id
     ReOb = re.get(url)
-    print(ReOb)
+   
+    #/debug
+    #print(ReOb)
 
     #Ueberpruefen, ob Status Code 200
     if(ReOb.status_code != 200):
@@ -46,5 +50,7 @@ def parsen():
     return Korri
 
 Korrelation = parsen()
-print(Korrelation.url)
-print(Korrelation.title)
+
+#/debug
+#print(Korrelation.url)
+#print(Korrelation.title)
