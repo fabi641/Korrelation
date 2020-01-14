23427f08506baddea81e9606eb9ceca1c087dbb3
Fabian Hartmann <git@hartmann.red>
2020-01-14 20:26:33 +0100

    twitter api und tweet erstell Funktion hinzugefuegt.

---
+++ tweet.py
@@ -0,0 +1,15 @@
+#Hier wird der Tweet gebaut aus dem inhalt, der von korrelation.py erzeugt wird
+
+import korrelation
+
+#Twitter Kram
+import twitter
+import keys
+api = twitter.Api(consumer_key=keys.consumer_key,
+                  consumer_secret=keys.consumer_secret,
+                  access_token_key=keys.access_token_key,
+                  access_token_secret=keys.access_token_secret)
+
+
+def tweet(picUrl, status):
+    api.PostUpdate(status, media = picUrl)
